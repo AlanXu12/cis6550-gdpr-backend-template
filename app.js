c2829dcc358b3c74cbd374cba29a20f39f9589ff
Pingfan Xu <pingfan.xu@mail.utoronto.ca>
2020-12-01 19:44:35 -0500

    added test code for simple request handling

--- app.js
+++ app.js
@@ -3,7 +3,18 @@ const express = require('express')
 const app = express();
 const port = 8000;
 
+// MongoDB instance
+let mongo = require('mongodb');
+let mongoClient = mongo.MongoClient;
+let url = "mongodb+srv://pingfan:pingfan@cluster0.iwciw.mongodb.net/test?authSource=admin&replicaSet=atlas-m7bgyt-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true"
+
+
 app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+app.post('/record', (req, res) => {
+  console.log("req.body:", req.body);
   res.send('Hello World!')
 });
 
