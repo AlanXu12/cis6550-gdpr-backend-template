a763c5dbab63d961c04bae50bb47da32dd5ddff2
Pingfan Xu <pingfan.xu@mail.utoronto.ca>
2020-12-02 01:30:20 -0500

    finished postrequest handler for adding record to db

--- app.js
+++ app.js
@@ -16,11 +16,7 @@ let mongo = require('mongodb');
 let mongoClient = mongo.MongoClient;
 let url = "mongodb+srv://pingfan:pingfan@cluster0.iwciw.mongodb.net/test?authSource=admin&replicaSet=atlas-m7bgyt-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true"
 
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-});
-
+// Handler for adding new record to service collection
 app.post('/record', jsonParser, async (req, res) => {
   // Check if consent is included and valid
   if (!req.body.consentExp || new Date(req.body.consentExp) <= new Date(Date.now())) {
@@ -32,30 +28,35 @@ app.post('/record', jsonParser, async (req, res) => {
     res.send('No username');
     return;
   }
-  // Convert date of birth to Date obj
+  // Convert all json element starts with "date" to Date obj
   let recordObj = req.body.record;
-  recordObj.dateOfBirth = new Date(recordObj.dateOfBirth);
+  for (rec in recordObj) {
+    if (rec.startsWith("date")) {
+      recordObj[rec] = new Date(recordObj[rec]);
+    }
+  }
   // Use username as the unique identifier
   recordObj._id = req.body.username;
+
+  // Connect to target DB
   const dbClient = await mongoClient.connect(url, { useUnifiedTopology: true }).catch((err) => {console.log("1st err");});
   if (!dbClient) return;
+
+  // Try to insert the new record to the target collection
   const dbObj = dbClient.db(req.body.db);
-  await dbObj.collection(req.body.collection).insertOne(recordObj).catch((err) => {console.log("last err"); return;});
-  // try {
-  //   let result = await dbObj.collection(req.body.collection).insertOne(recordObj);
-  // } catch (error) {
-  //   return res.status(402).end("err");
-  // }
+  const result = await dbObj.collection(req.body.collection).insertOne(recordObj).catch((err) => {res.status(400).send(err);});
+  if (!result) return;
+
+  // Try to insert a new record to central collection b/c new record added to target collection
   const recordCentralObj = {
     "username": req.body.username,
     "serviceCollection": req.body.collection,
-    "consentExp": req.body.consentExp
+    "consentExp": new Date(req.body.consentExp)
   };
-  await dbObj.collection("central").insertOne(recordCentralObj).catch((error) => {console.log("last err"); return;});
-  console.log("passed the last await");
+  const resultCentral = await dbObj.collection("central").insertOne(recordCentralObj).catch((error) => {res.status(400).send(err);});
+  if (!resultCentral) return;
   dbClient.close();
   return res.end('1 record inserted');
-
 });
 
 app.listen(port, () => {
